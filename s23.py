819b2b8853d97cbf690f6ae5330a98b32d25909c
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-06-11 13:38:29 +0200

    Update s23.py

--- s23.py
+++ s23.py
@@ -27,24 +27,3 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 # Print results.
 for row in rows:
     print(row.Betrag)
-
-
-
-
-""" # streamlit_app.py
-
-import pandas as pd
-import streamlit as st
-
-st.title("Finanzen Interrail")
-
-# Read in data from the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url, on_bad_lines='skip')
-
-df = load_data(st.secrets["public_gsheets_url"])
-
-st.dataframe(df) """
