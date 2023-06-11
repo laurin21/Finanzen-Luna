da0f808d1088e644b9b375d23dc3c92f5aa392ab
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-06-11 13:07:32 +0200

    Update s23.py

--- s23.py
+++ s23.py
@@ -1,5 +1,38 @@
 # streamlit_app.py
 
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
+
+
+
+
+""" # streamlit_app.py
+
 import pandas as pd
 import streamlit as st
 
@@ -14,4 +47,4 @@ def load_data(sheets_url):
 
 df = load_data(st.secrets["public_gsheets_url"])
 
-st.dataframe(df)
+st.dataframe(df) """
