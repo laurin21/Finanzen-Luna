54c5a3d84f5dac6c54352cac4f51ccf73fc55eb1
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-06-11 22:33:28 +0200

    Update s23.py

--- s23.py
+++ s23.py
@@ -30,12 +30,6 @@ days = 31
 total_budget = 3000
 daily_budget = round(total_budget / days, 2)
 
-days_list = ["18.07.2023", "19.07.2023", "20.07.2023", "21.07.2023", "22.07.2023", "23.07.2023", "24.07.2023", "25.07.2023", "26.07.2023", "27.07.2023", "28.07.2023", "29.07.2023", "30.07.2023", "31.07.2023", "01.08.2023", "02.08.2023", "03.08.2023", "04.08.2023", "05.08.2023", "06.08.2023", "07.08.2023", "08.08.2023", "09.08.2023", "10.08.2023", "11.08.2023", "12.08.2023", "13.08.2023", "14.08.2023", "15.08.2023", "16.08.2023", "17.08.2023"]
-sum_list = [0] * days 
-budget_list = [daily_budget] * days
-total_sum_list = [0] * days 
-diff_list = [0] * days 
-moving_budget_list = [0] * days 
 
 df = pd.DataFrame(rows, columns =['Datum', 'Beschreibung', 'Kategorie', 'Betrag', 'Split'])
 
@@ -50,6 +44,21 @@ df["Datum"] = pd.to_datetime(df["Datum"], format = "%d.%m.%Y", errors = "coerce"
 df["Betrag"] = df["Betrag"].str.replace(",",".")
 df["Betrag"] = df["Betrag"].astype('float')
 
+
+
+
+cats = df["Kategorie"].unique()
+
+sum_cats = df.groupby("Kategorie")["Betrag"].sum()
+sum_dates = df.groupby("Datum")["Betrag"].sum()
+
+days_list = ["18.07.2023", "19.07.2023", "20.07.2023", "21.07.2023", "22.07.2023", "23.07.2023", "24.07.2023", "25.07.2023", "26.07.2023", "27.07.2023", "28.07.2023", "29.07.2023", "30.07.2023", "31.07.2023", "01.08.2023", "02.08.2023", "03.08.2023", "04.08.2023", "05.08.2023", "06.08.2023", "07.08.2023", "08.08.2023", "09.08.2023", "10.08.2023", "11.08.2023", "12.08.2023", "13.08.2023", "14.08.2023", "15.08.2023", "16.08.2023", "17.08.2023"]
+sum_list = [0] * days 
+budget_list = [daily_budget] * days
+total_sum_list = [0] * days 
+diff_list = [0] * days 
+moving_budget_list = [0] * days 
+
 df_budget = pd.DataFrame(columns =['Datum', 'Betrag', 'Tagesbudget', 'Diff', "Gesamtbetrag",  "Moving Budget"])
 df_budget["Datum"] = days_list
 df_budget["Betrag"] = sum_list
@@ -59,16 +68,16 @@ df_budget["Gesamtbetrag"] = total_sum_list
 df_budget["Floating Budget"] = moving_budget_list
 df_budget["Datum"] = pd.to_datetime(df_budget["Datum"], format = "%d.%m.%Y", errors = "coerce")
 
+
+df_budget["Floating Budget"] = df['df_budget'].cumsum()
+
 st.dataframe(df_budget)
 
 #####
 
 st.title("Finanzen Interrail")
 
-cats = df["Kategorie"].unique()
 
-sum_cats = df.groupby("Kategorie")["Betrag"].sum()
-sum_dates = df.groupby("Datum")["Betrag"].sum()
 
 st.dataframe(sum_dates)
 st.bar_chart(sum_dates)
