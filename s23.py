5658aba740584587a6504016321e7333d593e498
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-06-11 12:07:24 +0200

    Update s23.py

--- s23.py
+++ s23.py
@@ -10,7 +10,7 @@ st.title("Finanzen Interrail")
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url, sep=';', header=True)
+    return pd.read_csv(csv_url, error_bad_lines=False)
 
 df = load_data(st.secrets["public_gsheets_url"])
 
