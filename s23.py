5e94bfe917ffae9d6308297f9e448385e7108d19
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-06-12 10:38:20 +0200

    Update s23.py

--- s23.py
+++ s23.py
@@ -78,7 +78,7 @@ df_budget["Diff"] = df_budget["Tagesbudget"] - df_budget["Ausgaben"]
 
 df_budget["Ausgaben Gesamt"] = df_budget['Ausgaben'].cumsum()
 
-df_budget["Moving Diff"] = df_budget[2]+df_budget[3]
+df_budget["Moving Diff"] = df_budget.loc[2]+df_budget.loc[3]
 
 df_budget["Moving Budget"] = df_budget['Tagesbudget'].cumsum() 
 
