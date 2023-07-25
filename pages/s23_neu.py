623618b5218b90baea8573cf723e270bfdcf4dfa
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-07-25 15:51:33 +0200

    Update s23_neu.py

--- pages/s23_neu.py
+++ pages/s23_neu.py
@@ -76,10 +76,10 @@ df_budget["Budget Gesamt"] = df_budget['Budget'].cumsum()
 df_budget["Gesamt Diff"] = df_budget["Budget Gesamt"] - df_budget["Betrag Gesamt"]
 
 df_city = pd.DataFrame(df_feed.groupby("Stadt")["Betrag"].sum())
-df_city = df_city.sort_values(by='Betrag', ascending = False, inplace=True)
+df_city.sort_values(by='Betrag', ascending = False, inplace=True)
 
 df_categories = pd.DataFrame(df_feed.groupby("Kategorie")["Betrag"].sum())
-df_categories = df_categories.sort_values(by='Betrag', ascending = False, inplace=True)
+df_categories.sort_values(by='Betrag', ascending = False, inplace=True)
 
 ##########################
 
