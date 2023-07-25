35adab2245c9a2e05ad7269a878ff0a7bf4661c0
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-07-25 15:25:13 +0200

    Update s23_neu.py

--- pages/s23_neu.py
+++ pages/s23_neu.py
@@ -61,13 +61,11 @@ df_days['Betrag_df2'].fillna(0, inplace=True)
 df_days['Betrag'] = df_days['Betrag_df1'] + df_days['Betrag_df2']
 df_days.drop(columns=['Betrag_df1', 'Betrag_df2'], inplace=True)
 
-#st.write(df_split["Betrag"][4])
-
 df = df_feed.copy()
 st.write(df)
 for index in range(len(df_split)):
-    amount_per_day = df_split["Betrag"][index] / days
-    daily_expenses = pd.DataFrame({'Datum': days_list, 'Beschreibung': df_split["Beschreibung"][index] + " (splitted)", 'Kategorie': df_split["Kategorie"][index], 'Betrag': amount_per_day})
+    amount_per_day = df_split.loc[index]["Betrag"] / days
+    daily_expenses = pd.DataFrame({'Datum': days_list, 'Beschreibung': df_split.loc[index]["Beschreibung"] + " (splitted)", 'Kategorie': df_split.loc[index]["Kategorie"], 'Betrag': amount_per_day})
     df = pd.concat([df, daily_expenses], ignore_index=True)
     df = df.sort_values(by='Datum').reset_index(drop=True)
 
