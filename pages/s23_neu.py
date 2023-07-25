924cc5c00698411653db4148ca3409349c8e4c80
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-07-25 14:41:20 +0200

    Update s23_neu.py

--- pages/s23_neu.py
+++ pages/s23_neu.py
@@ -64,7 +64,7 @@ st.write("df_no_split")
 st.write(df_no_split)
 
 
-df_days = df_no_split.merge(df_days, on='Datum', how='left', suffixes=('_df1', '_df2'))
+df_days = df_days.merge(df_no_split, on='Datum', how='left', suffixes=('_df1', '_df2'))
 
 # Fill missing values in "Value_df2" column with 0 (to handle days without data in df2)
 df_days['Betrag_df2'].fillna(0, inplace=True)
