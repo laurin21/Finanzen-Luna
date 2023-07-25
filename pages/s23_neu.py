609173826371fe728cd7ab0720d2a0086d097231
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-07-25 15:41:06 +0200

    Update s23_neu.py

--- pages/s23_neu.py
+++ pages/s23_neu.py
@@ -40,10 +40,11 @@ df_feed["Stadt"] = df_feed["Stadt"].astype("category")
 df_feed["Datum"] = pd.to_datetime(df_feed["Datum"], format = "%d.%m.%Y", errors = "coerce").dt.date 
 df_feed["Betrag"] = df_feed["Betrag"].str.replace(",",".")
 df_feed["Betrag"] = df_feed["Betrag"].astype('float')
+df_feed['Stadt'] = df_feed['Stadt'].fillna("Split")
 
 days_list = ["18.07.2023", "19.07.2023", "20.07.2023", "21.07.2023", "22.07.2023", "23.07.2023", "24.07.2023", "25.07.2023", "26.07.2023", "27.07.2023", "28.07.2023", "29.07.2023", "30.07.2023", "31.07.2023", "01.08.2023", "02.08.2023", "03.08.2023", "04.08.2023", "05.08.2023", "06.08.2023", "07.08.2023", "08.08.2023", "09.08.2023", "10.08.2023", "11.08.2023", "12.08.2023", "13.08.2023", "14.08.2023", "15.08.2023", "16.08.2023", "17.08.2023"]
 
-df_split = df_feed[df_feed["Stadt"] == None]
+df_split = df_feed[df_feed["Stadt"] == "Split"]
 splitted = round(float(df_split["Betrag"].sum()) / days, 2)
 splitted_per_day = [splitted]*days
 df_split.reset_index(drop=True, inplace=True)
@@ -94,9 +95,13 @@ st.write("Budget:")
 st.write(df_budget)
 st.write("Stadt:")
 st.write(df_city)
+st.markdown("---")
+
+##########################
 
 
-
+st.title("Finanzen Interrail")
+st.markdown("### Ausgaben pro Tag")
 
 
 ##########################
