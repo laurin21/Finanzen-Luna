1d7335d9558cd6818796fa3befb10c1dec380aa0
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-12-04 15:56:23 +0100

    Update s23.py

--- s23.py
+++ s23.py
@@ -5,16 +5,28 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 import datetime as dt
-from streamlit_gsheets import GSheetsConnection
-
 
 #############
 
 # Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
-# Fetch existing vendors data
-rows = conn.read(worksheet="Buecher", usecols=list(range(6)), ttl=5)
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_resource(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 ##########################
 days = 31
